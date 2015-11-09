21d37105049f9e2205fa26f0b831eafb29e2daf7
prabah <prabah@gmail.com>
2015-11-09 06:13:41 +0000

    Initial commit

---
+++ Core/GetCurrencies.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class GetCurrencies : IGetCurrencies
+    {
+        public IList<string> GetCurrency()
+        {
+            return new List<string>();
+        }
+    }
+}
